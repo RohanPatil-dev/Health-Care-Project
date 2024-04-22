eb7fb98f184c4bcedbd98b5d06c53d2598d8c631
Rohan Patil <rohanpatil5122001@gmail.com>
2024-04-22 18:35:24 +0530

    Array of object created and mapping inside the Blogs and Services

---
+++ src/Main Project/Data/Blogs.js
@@ -0,0 +1,22 @@
+
+const Blog = [{
+    id : 1,
+    image : "./Images/blogs/blog_1.jpg",
+    blog_name : "Epilepsy Can be managed with Medical Support and Lifestyle Modifications",
+    description : "Manage recurrent seizures effectively with proper sleep, diet,exercise, and medication adherence", 
+    link : "../HTML/blogs_1.html"
+},{
+    id : 2,
+    image : "./Images/blogs/blog_2.jpg",
+    blog_name : "Can Weight Lifting Cause Cardiac Problems?",
+    description : "Improper techniques or overexertion can place unnecessary stress on the heart, emphasizing the significance of receiving proper training",
+    link : "../HTML/blogs_2.html"
+},{
+    id : 3,
+    image : "./Images/blogs/blog_3.jpg",
+    blog_name : "Unseen Threats: Detecting Diabetes Symptoms in Women",
+    description : "In women, diabetes symptoms include fatigue, frequent urination,blurred vision, and yeast infections, impacting overall health & well-being.",
+    link : "../HTML/blogs_3.html"
+}] 
+
+module.exports = {Blog}
---
+++ src/Main Project/Data/Services.js
@@ -0,0 +1,47 @@
+
+const services = [{
+    id : 1,
+    image : "./Images/Services/first-aid.png",
+    alt : "Card image cap",
+    service_name : "Free Checkup",
+    description : "First aid refers to medical attention that is usually administered immediately after the injury occurs and at the location where it occurred. It often consists of a one-time, short-term treatment and requires little technology or training to administer.",
+    link : "../HTML/Free checkup.html"
+},
+{
+    id : 2,
+    image : "./Images/Services/ambulance.png",
+    alt : "Card image cap",
+    service_name : "24/7 Ambulance",
+    description : "Central Health Care's Ambulance Services in all over India is one of the leading businesses in the Ambulance Services. Also known for Ambulance Services, Domestic Ambulance Services, Dead Body Ambulance Services, Ventilator Ambulance Services.",
+    link : "../HTML/Ambulance.html"
+},{
+    id : 3,
+    image : "./Images/Services/doctor-2.png",
+    alt : "Card image cap",
+    service_name : "Experts Consultancy",
+    description : "Doctors diagnose disease, provide treatment, counsel patients with injuries, diseases or illnesses. The specific duties depend upon the speciality you pursue in your MBBS. Some doctors work in cardiology, whereas others may work in surgery , neurology,pulmonology or rheumatology.",
+    link : "../HTML/Doctors.html"
+},{
+    id : 4,
+    image : "./Images/Services/medicine.png",
+    alt : "Card image cap",
+    service_name : "Medicines",
+    description : "Prevention is better than cure,A stitch in time saves nine are well-known adages. Today more emphasis is placed on the prevention of diseases and promoting health awareness.",
+    link : "../HTML/medicines.html"
+},{
+    id : 5,
+    image : "./Images/Services/bed-2.png",
+    alt : "Card image cap",
+    service_name : "Bed Facility",
+    description : "Practically every function and piece of the bed will be checked in a service. This is to ensure that the hospital bed is still performing as well as it should be, and to make sure that patients and staff are safe to use it. The service engineer will carry out some visual checks on the bed first.",
+    link : "../HTML/Beds.html"
+},{
+    id : 6,
+    image : "./Images/Services/heartbeat.png",
+    alt : "Card image cap",
+    service_name : "Total Care",
+    description : "Total patient care is a model of care overseen by a registered nurse who provides one-on-one care to patients. These RNs work with you to create a personalized, whole-person care plan for your loved one.",
+    link : "../HTML/Total care.html"
+}]
+
+module.exports = {services}
