441308559b4db814de24cb9dbb111fbba3e4ae15
Rohan Patil <rohanpatil5122001@gmail.com>
2024-05-12 12:00:29 +0530

    doctor checkup module has created successfully

--- src/Main Project/UI/Doctor/Doctor_checkup.jsx
+++ src/Main Project/UI/Doctor/Doctor_checkup.jsx
@@ -1,45 +1,60 @@
 import React from "react"
 import Doctor_Nav from "./Doctor reusable components/Doctor_Nav"
+import Footer from "../Index/Footer"
 
 export default function Doctor_checkup() {
     return (
         <>
+
             <Doctor_Nav />
+            <div>
+                <div className="container doc_container">
 
-            <div className="container">
+                    <form action="" method="post">
+                        <div className="doc_checkup">
+                            <div class="form-group">
+                                <label for="names">Name</label>
+                                <input type="text" class="form-control" id="names" aria-describedby="emailHelp" placeholder="Enter name" />
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail" placeholder="Enter Email" />
+                            </div>
 
-                <form action="" method="post">
-                <div className="doc_checkup">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                    </div>
+                            <div class="form-group">
+                                <label for="appointment">Next appointment</label>
+                                <input type="datetime-local" class="form-control" id="appointment" placeholder="Enter Next Appointment" />
+                            </div>
 
-                   <div id="form_grid">
-                   <div>
-                        <select className="custom-select">
-                            <option selected value={"none"}>Select Ward</option>
-                            <option value="General">General</option>
-                            <option value="General">General</option>
-                            <option value="Emergency">Emergency</option>
-                            <option value="ICU">ICU</option>
-                        </select>
-                    </div>
+                            <div>
+                                <label htmlFor="">Select ward</label>
+                                <select className="custom-select select_doc">
+                                    <option selected value={"none"}>Select Ward</option>
+                                    <option value="General">General</option>
+                                    <option value="General">General</option>
+                                    <option value="Emergency">Emergency</option>
+                                    <option value="ICU">ICU</option>
+                                </select>
+                            </div>
 
-                    <div className="form-group">
-                        <label htmlFor="bed">Select Bed Number</label>
-                        <input type="number" className="form-control" id="bed" maxLength={1} minLength={100} placeholder="Select Bed" />
-                    </div>
-                   </div>
-                   </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-       
+                            <div className="form-group">
+                                <label htmlFor="beds">Select Bed Number</label>
+                                <input type="number" className="form-control" id="beds" maxLength={1} minLength={100} placeholder="Select Bed" />
+                            </div>
+                        </div>
+
+                        <div class="form-group" id="doc_textarea">
+                            <label for="exampleFormControlTextarea1">Prescriptions for Patients</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Write something here......"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary doc_btn">Submit</button>
+                    </form>
+
+                </div>
+
             </div>
+            <Footer />
         </>
     )
 }
