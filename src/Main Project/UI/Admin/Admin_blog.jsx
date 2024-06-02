0bb80b0eed0ddb41d8942d82cc71e0df16af1dbe
Rohan Patil <rohanpatil5122001@gmail.com>
2024-06-02 22:50:57 +0530

    admin blog form refactored successfully

--- src/Main Project/UI/Admin/Admin_blog.jsx
+++ src/Main Project/UI/Admin/Admin_blog.jsx
@@ -25,22 +25,22 @@ export default function Admin_blog() {
             <form action="" method="post">
               <div className="blog_inputs">
                 <div class="form-group">
-                  <label for="exampleInputPassword1">Enter your title</label>
-                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                  <label for="exampleInputPassword1" className="tag">Enter your title</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter your title........." />
                 </div>
 
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Add Image</label>
-                  <input type="file" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                  <label for="exampleInputEmail1" className="tag">Add Image</label>
+                  <input type="file" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Add image" />
                 </div>
               </div>
 
               <div class="form-group" id="editor">
-                <label for="exampleFormControlTextarea1">Add decription</label>
+                <label for="exampleFormControlTextarea1" className="tag">Add decription</label>
                 <JoditEditor ref={editor} value={content} onChange={newContent => setContent(newContent)} />
               </div>
 
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" class="btn blog-btn">Submit</button>
             </form>
           </div>
         </div>
